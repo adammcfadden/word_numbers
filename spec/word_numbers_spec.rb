87df13e7f2a0b1b5e89be629c4af1dbaaefcbcde
David & Adam <student@epicodus.com>
2015-04-21 15:04:39 -0700

    created a second spec to check that the number of loops used is correct.

--- spec/word_numbers_spec.rb
+++ spec/word_numbers_spec.rb
@@ -3,7 +3,12 @@ require('word_numbers')
 
 describe('String#word_numbers') do
   it('return the correct word version of the input number') do
-    expect(('81').word_numbers()).to(eq('eighty one'))
-
+    expect(("81").word_numbers()).to(eq('eighty one'))
   end
+
+it('determine number of loops required') do
+  expect(("975").word_numbers()).to(eq(3))
+
+end
+
 end
