903356d0639f48b582ca7f8e8a345dedd2fab96d
David & Adam <student@epicodus.com>
2015-04-21 14:04:19 -0700

    Initial commit, Added purpose to README and first spec testing number input to word output

---
+++ README.md
@@ -0,0 +1,1 @@
+receive a number input, and translate that to a word version of that number. (ex: 81 = eighty one)
---
+++ spec/word_numbers_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('word_numbers')
+
+describe('String#word_numbers') do
+  it('return the correct word version of the input number') do
+    expect((81).word_numbers()).to(eq('eighty one'))
+
+  end
+end
