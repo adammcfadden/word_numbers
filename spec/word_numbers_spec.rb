6aea8b16f45a02870923a1573b9aeebf68607256
David & Adam <student@epicodus.com>
2015-04-21 16:30:02 -0700

    Passed first spec of returning a single number. Added spec for a two digit number.

--- spec/word_numbers_spec.rb
+++ spec/word_numbers_spec.rb
@@ -1,14 +1,13 @@
 require('rspec')
 require('word_numbers')
 
-describe('String#word_numbers') do
-  it('return the correct word version of the input number') do
-    expect(("81").word_numbers()).to(eq('eighty one'))
+describe('Fixnum#word_numbers') do
+  it('return the correct word for a ones place number') do
+    expect((1).word_numbers()).to(eq("one"))
   end
 
-it('determine number of loops required') do
-  expect(("975").word_numbers()).to(eq(3))
-
-end
+  it('return the correct word version of the input number in the teens place') do
+    expect((17).word_numbers()).to(eq('seventeen'))
+  end
 
 end
